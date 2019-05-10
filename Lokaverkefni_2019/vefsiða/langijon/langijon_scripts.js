177a34a85eb264702ecef7182d37b1aee62c74ad
Zkur1 <askur000@gmail.com>
2019-05-10 20:53:48 +0000

    update

---
+++ Lokaverkefni_2019/vefsiða/langijon/langijon_scripts.js
@@ -0,0 +1,9 @@
+
+var firestore = firestore.firestore()
+const docRef =
+button_history.mousePressed(changeHeader);
+
+function changeHeader(){
+    var tool_description = document.getElementById("description");
+    tool_description.innerHTML += "Sæll Viiiinur! ";
+}
---
+++ Lokaverkefni_2019/vefsiða/starfsmenn/starfsmenn_scripts.js
@@ -0,0 +1,8 @@
+
+var firestore = firestore.firestore()
+const docRef =
+staff_img.mousePressed(goToStaff);
+
+function goToStaff(){
+    window.open("staff/reynir.html", '_self')
+}
