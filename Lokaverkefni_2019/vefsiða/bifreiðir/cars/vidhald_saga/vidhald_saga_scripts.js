e9da16f92bc5c91c9dd6a9728df69766cf3c2179
Zkur1 <askur000@gmail.com>
2019-08-13 17:54:15 +0000

    vidhald_history

--- Lokaverkefni_2019/vefsiða/bifreiðir/cars/vidhald_saga/vidhald_saga_scripts.js
+++ Lokaverkefni_2019/vefsiða/bifreiðir/cars/vidhald_saga/vidhald_saga_scripts.js
@@ -1,40 +1,115 @@
 // Reads through the subcollection "in_out" of the specific tool, snapshots its contents and runs the function "renderCheckInData" for each document.
 // Note: function "orderBy()" lists the objects by "checkOutDate in decending order.
 // Note: function "slice()" limits the "forEach()" request by a specified amount (in this case it limits it to 10 results). 
-firestore.collection('Tools').doc(localStorage.getItem("tool_selector")).collection("in_out").orderBy('checkOutDate', 'desc').get().then((snapshot) => { 
+firestore.collection('Cars').doc(localStorage.getItem("car_selector")).collection("checkup_history").orderBy('dateCreated', 'desc').get().then((snapshot) => { 
     snapshot.docs.forEach(doc => {
-            renderCheckInData(doc);
+            renderCheckupData(doc);
     });
 });
 
+firestore.collection('Cars').doc(localStorage.getItem("car_selector")).collection("oil_change_history").orderBy('dateCreated', 'desc').get().then((snapshot) => { 
+    snapshot.docs.forEach(doc => {
+            renderOilChangeData(doc);
+    });
+});
+
+firestore.collection('Cars').doc(localStorage.getItem("car_selector")).collection("tire_change_history").orderBy('dateCreated', 'desc').get().then((snapshot) => { 
+    snapshot.docs.forEach(doc => {
+            renderTireChangeData(doc);
+    });
+});
+
+
 // Renders all history data of the tool and displays on the page. 
-function renderCheckInData(doc){
+function renderCheckupData(doc){
     // Creates elements.
     let li = document.createElement('li');
-    let check_in = document.createElement('span');
-    let check_out = document.createElement('span');
-    let projectID_out = document.createElement('span');
+    let checkup_date_text = document.createElement('span');
+    let checkup_date_var = document.createElement('span');
+    let checkup_staff_text = document.createElement('span');
+    let checkup_staff_var = document.createElement('span');
     
     // Sets specific content in id to each element.
     li.setAttribute("id", doc.id);
-    check_out.setAttribute("id", "check_out");
-    check_out.setAttribute("class", "check_out");
-    check_in.setAttribute("id", "check_in");
-    check_in.setAttribute("class", "check_in");
-    projectID_out.setAttribute("id", "projectID_out");
-    projectID_out.setAttribute("class", "projectID_out");
-
-    check_out.textContent = "Verkfæri skráð út þann: " + doc.data().checkOutDate + " af starfsmanni: " +  doc.data().checkOutUser + " \n";
-    check_in.textContent = "Verkfæri skráð inn þann: " + doc.data().checkInDate + " af starfsmanni: " +  doc.data().checkInUser + " \n";
-    projectID_out.textContent = "Verkfæri skráð á verknúmer: " + doc.data().checkOutProject;
+    checkup_date_text.setAttribute("id", "checkup_date");
+    checkup_date_text.setAttribute("class", "checkup_date");
+    checkup_date_var.setAttribute("class", "checkup_bold");  
+    checkup_staff_var.setAttribute("class", "checkup_bold"); 
+    
+    checkup_date_var.style.fontWeight = "bold";    
+    checkup_staff_var.style.fontWeight = "bold";
+    
+    checkup_date_text.textContent =  "Síðasta skoðun: ";
+    checkup_date_var.textContent = doc.data().checkupDate + "\n";
+    checkup_staff_text.textContent = "Viðstaddur starfsmaður: ";
+    checkup_staff_var.textContent = doc.data().checkupStaff + " \n";
 
     // Appends content into li.
-    li.appendChild(check_out);
-    li.appendChild(check_in);
-    li.appendChild(projectID_out);
+    li.appendChild(checkup_date_text);
+    li.appendChild(checkup_date_var);
+    li.appendChild(checkup_staff_text);
+    li.appendChild(checkup_staff_var);
 
     // Appends li to the tool_list (ul)
-    document.getElementById("check_in_list").appendChild(li);
+    document.getElementById("checkup_list").appendChild(li);
+
+    // Enables scrolling inside of a spescific div.
+    document.addEventListener("touchstart", function(){}, true)
+}
+
+
+// Renders all history data of the tool and displays on the page. 
+function renderOilChangeData(doc){
+    // Creates elements.
+    let li = document.createElement('li');
+    let oil_change_km = document.createElement('span');
+    let oil_change_staff = document.createElement('span');
+    
+    // Sets specific content in id to each element.
+    li.setAttribute("id", doc.id);
+    oil_change_km.setAttribute("id", "oil_change_km");
+    oil_change_km.setAttribute("class", "oil_change_km");
+    oil_change_staff.setAttribute("id", "oil_change_staff");
+    oil_change_staff.setAttribute("class", "oil_change_staff");
+
+    oil_change_km.textContent = doc.data().oilChangeKm + " \n";
+    oil_change_staff.textContent = doc.data().dateCreated + " \n";
+
+    // Appends content into li.
+    li.appendChild(oil_change_km);
+    li.appendChild(oil_change_staff);
+
+    // Appends li to the tool_list (ul)
+    document.getElementById("oil_change_list").appendChild(li);
+
+    // Enables scrolling inside of a spescific div.
+    document.addEventListener("touchstart", function(){}, true)
+}
+
+
+// Renders all history data of the tool and displays on the page. 
+function renderTireChangeData(doc){
+    // Creates elements.
+    let li = document.createElement('li');
+    let tire_change_date = document.createElement('span');
+    let tire_change_staff = document.createElement('span');
+    
+    // Sets specific content in id to each element.
+    li.setAttribute("id", doc.id);
+    tire_change_date.setAttribute("id", "tire_change_date");
+    tire_change_date.setAttribute("class", "tire_change_date");
+    tire_change_staff.setAttribute("id", "tire_change_staff");
+    tire_change_staff.setAttribute("class", "tire_change_staff");
+
+    tire_change_date.textContent = doc.data().tireChangeDate + " \n";
+    tire_change_staff.textContent = doc.data().dateCreated + " \n";
+
+    // Appends content into li.
+    li.appendChild(tire_change_date);
+    li.appendChild(tire_change_staff);
+
+    // Appends li to the tool_list (ul)
+    document.getElementById("tire_change_list").appendChild(li);
 
     // Enables scrolling inside of a spescific div.
     document.addEventListener("touchstart", function(){}, true)
