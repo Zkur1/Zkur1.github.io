eb405d7f162d257b0a103c7cfff6fb184b7f9d59
Zkur1 <askur000@gmail.com>
2020-01-14 21:18:08 +0000

    lagfæringar (m.a footer)

--- tar.is/website/config/footer/footer.js
+++ tar.is/website/config/footer/footer.js
@@ -1,7 +1,20 @@
 // Builds the menu overlay by adding html code to the already created 'menuOverlay' element.
-document.getElementById("menuOverlay").innerHTML += `
-    <a href='/tar.is/website/'>Heim</a>
-    <a href='/tar.is/website/hugbunadur/hugbunadur.html'>Hugbúnaðarlausnir</a>
-    <a href='/tar.is/website/umOkkur/umOkkur.html'>Um okkur</a>
-    <a href='/tar.is/website/hafaSamband/hafaSamband.html'>Hafa samband</a>
+document.querySelector('footer').innerHTML += `
+    <div class='footerInfo'>
+        <h2>TAR hugbúnaðarlausnir</h2>
+        <div class='footerDetail'>
+            <div class='emailPhone'>
+                <h3>+354-696-8868</h3>
+                <h3>tar@tar.is</h3>
+            </div>
+            <div class='location'>
+                <h3>Reykjavík,</h3>
+                <h3>Iceland</h3>
+            </div>
+        </div>
+    </div>
+    <h4 id='copyright' class='copyright'></h4>
+    
 `
+
+document.getElementById('copyright').innerHTML = '© ' + new Date().getFullYear() + ' Tar ehf.'; 
